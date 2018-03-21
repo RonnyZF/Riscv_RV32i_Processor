3f89b6930b1619ad4392ef654e605e29a8bc38bc
Ronny Zárate <31499007+RonnyZF@users.noreply.github.com>
2018-03-20 23:17:02 -0600

    Update ID_EXE_PIPELINE.sv

--- ID_EXE_PIPELINE.sv
+++ ID_EXE_PIPELINE.sv
@@ -18,22 +18,19 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-
 module ID_EXE_PIPELINE(
     input rst,
     input clk,
-    input [1:0] CRT_WB_IN,
-    input [2:0] CRT_MEM_IN,
-    input [2:0] CRT_EXE_IN,
+    input [1:0]  CRT_WB_IN,
+    input [2:0]  CRT_MEM_IN,
+    input [2:0]  CRT_EXE_IN,
     input [31:0] PC_IN,
     input [31:0] DATA_A_IN,
     input [31:0] DATA_B_IN,
     input [31:0] DATA_SE_IN,
-    input [4:0] INST_IN,
-    
-    input [6:0] FUNCT7_IN,
-    input [2:0] FUNCT3_IN,
+    input [4:0]  INST_IN,
+    input [6:0]  FUNCT7_IN,
+    input [2:0]  FUNCT3_IN,
     
     output reg [1:0]  CRT_WB_OUT,
     output reg [2:0]  CRT_MEM_OUT,
@@ -43,66 +40,36 @@ module ID_EXE_PIPELINE(
     output reg [31:0] DATA_B_OUT,
     output reg [31:0] DATA_SE_OUT,
     output reg [4:0]  INST_OUT, 
-    
     output reg [6:0] FUNCT7_OUT,
     output reg [2:0] FUNCT3_OUT   
     );
     
-    reg  [1:0] CRT_WB_PIPELINE;
-    reg  [2:0] CRT_MEM_PIPELINE;
-    reg  [2:0] CRT_EXE_PIPELINE;
-    reg  [31:0] PC_IN_PIPELINE;
-    reg  [31:0] DATA_A_PIPELINE;
-    reg  [31:0] DATA_B_PIPELINE;
-    reg  [31:0] DATA_SE_PIPELINE;
-    reg  [4:0] INST_PIPELINE;
-    
-    reg [6:0] FUNCT7_PIPELINE;
-    reg [2:0] FUNCT3_PIPELINE;
-    
-    
-    always @ (negedge clk)
-    begin
-        if (rst==1)
-        begin
-        CRT_WB_PIPELINE <= 2'd0;
-        CRT_MEM_PIPELINE <= 3'b000;
-        CRT_EXE_PIPELINE <= 3'd0;
-        PC_IN_PIPELINE <= 32'd0;
-        DATA_A_PIPELINE <= 32'd0;
-        DATA_B_PIPELINE <= 32'd0;
-        DATA_SE_PIPELINE <= 16'd0;
-        INST_PIPELINE  <= 5'd0;
-        FUNCT7_PIPELINE <= 7'd0;
-        FUNCT3_PIPELINE <= 3'd0;
-        end
+    always @ (posedge clk)
+        if(rst)
+            begin
+            CRT_WB_OUT  <= 2'd0;   
+            CRT_MEM_OUT <= 3'd0;   
+            CRT_EXE_OUT <= 3'd0;  
+            PC_OUT      <= 32'd0;  
+            DATA_A_OUT  <= 32'd0;  
+            DATA_B_OUT  <= 32'd0;  
+            DATA_SE_OUT <= 32'd0;  
+            INST_OUT    <= 5'd0; 
+            FUNCT7_OUT  <= 7'd0; 
+            FUNCT3_OUT  <= 3'd0;
+            end
         else
             begin
-            CRT_WB_PIPELINE  <= CRT_WB_IN;    
-            CRT_MEM_PIPELINE <= CRT_MEM_IN;   
-            CRT_EXE_PIPELINE <= CRT_EXE_IN;   
-            PC_IN_PIPELINE   <= PC_IN;    
-            DATA_A_PIPELINE  <= DATA_A_IN;   
-            DATA_B_PIPELINE  <= DATA_B_IN;   
-            DATA_SE_PIPELINE <= DATA_SE_IN;  
-            INST_PIPELINE    <= INST_IN;
-            FUNCT7_PIPELINE <= FUNCT7_IN;
-            FUNCT3_PIPELINE <= FUNCT3_IN;
-            end    
-        end  
-    always @ (posedge clk)
- 
-        begin
-        CRT_WB_OUT  <= CRT_WB_PIPELINE;    
-        CRT_MEM_OUT <= CRT_MEM_PIPELINE;   
-        CRT_EXE_OUT <= CRT_EXE_PIPELINE;  
-        PC_OUT   <= PC_IN_PIPELINE;    
-        DATA_A_OUT  <= DATA_A_PIPELINE;   
-        DATA_B_OUT  <= DATA_B_PIPELINE;   
-        DATA_SE_OUT <= DATA_SE_PIPELINE;  
-        INST_OUT    <= INST_PIPELINE;
-        FUNCT7_OUT <= FUNCT7_PIPELINE;
-        FUNCT3_OUT <= FUNCT3_PIPELINE;      
-    end
+            CRT_WB_OUT  <= CRT_WB_IN;    
+            CRT_MEM_OUT <= CRT_MEM_IN;   
+            CRT_EXE_OUT <= CRT_EXE_IN;  
+            PC_OUT      <= PC_IN;       
+            DATA_A_OUT  <= DATA_A_IN;   
+            DATA_B_OUT  <= DATA_B_IN;   
+            DATA_SE_OUT <= DATA_SE_IN;  
+            INST_OUT    <= INST_IN;    
+            FUNCT7_OUT  <= FUNCT7_IN;  
+            FUNCT3_OUT  <= FUNCT3_IN;      
+            end
 
 endmodule
