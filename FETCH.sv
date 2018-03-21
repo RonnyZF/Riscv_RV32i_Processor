3d13167afbed06ec09f5607b296a7930e05fd5c5
Ronny Zárate <31499007+RonnyZF@users.noreply.github.com>
2018-03-21 11:52:36 -0600

    Update FETCH.sv

--- FETCH.sv
+++ FETCH.sv
@@ -19,37 +19,36 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 module FETCH(
-    input [31:0] PC_MEM,
     input clk,
     input rst,
+    input [31:0] PC_MEM,
     input MUX_CRT,
     output [31:0] PC_OUT,
     output [31:0] DATA_OUT
     );
     reg [31:0] PC_FETCH;
     reg [31:0] PC_MUX;
-    //reg [31:0] PC;
     reg [31:0] DATA;
     
-    InstructionMem INST_MEM (.Address(PC_MUX[31:0]),.Word(DATA));
+    InstructionMem INST_MEM (.clk(clk),.Address(PC_MUX[31:0]),.Word(DATA));
     
-    always @ * begin
+    always @ (posedge clk) 
+    begin
        case (MUX_CRT)
          1'b0: PC_MUX <= PC_FETCH;
          1'b1: PC_MUX <= PC_MEM;
-         //default: PC_MUX = 32'b00000000000000000000000000000001;
-   endcase
+       endcase
    end  
 
-    always @ (posedge clk) begin
-            if (rst)
-             PC_FETCH = 32'd0;
-             else
-             begin
+    always @ (posedge clk)
+    begin
+        if (rst)
+            PC_FETCH = 32'd0;
+        else
             PC_FETCH = PC_MUX + 32'd4;
-            end
-            end
+    end
             
     assign DATA_OUT = DATA;
     assign PC_OUT = PC_MUX;
+    
 endmodule
